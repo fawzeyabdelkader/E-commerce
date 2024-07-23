e1803855ffda113fa4cfb4483d241f40be547284
week-2 <fawzeyabdelkader32@gmail.com>
2024-07-23 10:49:50 +0300

    fix CategoriesComponent

--- src/app/components/categories/categories.component.ts
+++ src/app/components/categories/categories.component.ts
@@ -9,15 +9,20 @@ import { ProductService } from 'src/app/services/product-service.service';
 })
 export class CategoriesComponent implements OnInit{
   AllCategories:IProduct[]=[];
+  isLoading:boolean=false;
 constructor(private _ProductService:ProductService){}
   ngOnInit(): void {
+    this.isLoading=true;
     this._ProductService.getAllCategories().subscribe({
       next: (response) => {
         console.log(response)
         this.AllCategories=response.data
+        this.isLoading=false;
       },
       error: (err) => {
         console.log(err);
+        this.isLoading=false;
+
       }
     })
   }
